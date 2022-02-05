d2b7ff9b553828663a51f4faf4dc3a919175fba5
Micheala Palmer <michealapalmer@gmail.com>
2022-02-04 19:21:58 -0500

    update users template and add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
